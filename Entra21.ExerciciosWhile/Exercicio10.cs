a82a4f0e3eac1cd531b37807942269d7d6cdba92
Gustavo Lemos <gustavo.aulas@outlook.com>
2022-05-06 21:45:44 -0300

    Exercício 10 criado. Exercício criado com base na lista de exercícios while.

---
+++ Entra21.ExerciciosWhile/Exercicio10.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosWhile
+{
+    internal class Exercicio10
+    {
+        public void Executar()
+        {
+            Console.Write("Digite um número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int antecessorPar = 0;
+            int sucessorImpar = 0;
+            string listaAntecessorPar = "";
+            string listaSucessorImpar = "";
+            int indice = 0;
+            if ((numero % 2) == 0)
+            {
+                antecessorPar = numero - 2;
+                sucessorImpar = numero + 1;
+                while (indice <= 49)
+                {
+                    listaAntecessorPar = listaAntecessorPar + "\n" + antecessorPar;
+                    listaSucessorImpar = listaAntecessorPar + "\n" + antecessorPar;
+                    antecessorPar = antecessorPar - 2;
+                    sucessorImpar = sucessorImpar + 2;
+                    indice = indice + 1;
+                }
+            }
+            else
+            {
+                antecessorPar = numero - 2;
+                sucessorImpar = numero + 2;
+                while (indice <= 49)
+                {
+                    listaAntecessorPar = listaAntecessorPar + "\n" + antecessorPar;
+                    listaSucessorImpar = listaAntecessorPar + "\n" + antecessorPar;
+                    antecessorPar = antecessorPar - 2;
+                    sucessorImpar = sucessorImpar + 2;
+                    indice = indice + 1;
+                }
+            }
+            Console.Write("Os 50 números antecessores pares, são:" + "\n" +
+                listaAntecessorPar + "\nOs 50 números sucessores ímpares, são:" +
+                "\n" + listaSucessorImpar);
+
+        }
+
+    }
+}
--- Entra21.ExerciciosWhile/Program.cs
+++ Entra21.ExerciciosWhile/Program.cs
@@ -1,9 +1,9 @@
 ﻿using Entra21.ExerciciosWhile;
 
 Console.WriteLine(@"---------Menu--------------
-1 - Exemplo 01
-2 - Exemplo 02
-3 - Exemplo 03");
+1 - Exercício 01
+2 - Exercício 02
+3 - Exercício 03");
 
 Console.Write("Digite a opção desejada dasdsa: ");
 int opcaoDesejada = Convert.ToInt32(Console.ReadLine());
@@ -18,4 +18,9 @@ else if (opcaoDesejada == 2)
 {
     Exercicio09 exercicio09 = new Exercicio09();
     exercicio09.Executar();
+}
+else if (opcaoDesejada == 3)
+{
+    Exercicio10 exercicio10 = new Exercicio10();
+    exercicio10.Executar();
 }
