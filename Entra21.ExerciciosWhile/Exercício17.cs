eea466a4f41a9f4a683a62ded4a18f2b02665599
GustavoLemoos <gustavo.aulas@outlook.com>
2022-05-10 15:31:11 -0300

    Exercício 17. Resolução do exercício 17.

--- Entra21.ExerciciosWhile/Exercício17.cs
+++ Entra21.ExerciciosWhile/Exercício17.cs
@@ -12,20 +12,34 @@ namespace Entra21.ExerciciosWhile
         {
             Console.Write("Temporizador: ");
             int segundos = 59, minutos = 59, horas = 23;
-            int indiceSegundos = 0, indiceMinutos = 0, indiceHoras = 0;
+            int indiceSegundos = 59, indiceMinutos = 59, indiceHoras = 3600;
             int indice = 0;
             while (indice < 86399)
             {
-                Console.Write(horas + ":" + minutos + ":" + segundos);
+                Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+                indiceSegundos = indiceSegundos - 1;
                 segundos = segundos - 1;
-                if ((segundos < 0))
+                if (indiceSegundos < 0)
                 {
+                    indiceSegundos = 59;
                     segundos = 59;
+                    indiceMinutos = indiceMinutos - 1;
+                    minutos = minutos - 1;
                 }
                 else
                 { }
-                indiceMinutos = indiceMinutos + 1;
-                if ((indiceMinutos%3600)  1 )
+                if (indiceMinutos < 0)
+                {
+                    indiceMinutos = 60;
+                    minutos = 59;
+                }
+                else { }
+                indiceHoras = indiceHoras - 1;
+                if (indiceHoras < 0)
+                {
+                    indiceHoras = 3600;
+                    horas = horas - 1;
+                }
                 indice = indice + 1;
             }
         }
