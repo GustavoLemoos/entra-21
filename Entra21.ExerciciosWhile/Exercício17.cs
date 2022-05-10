0e3cbcb1a633af1fc73e6a1721288149c59a823f
Gustavo Lemos <gustavo.aulas@outlook.com>
2022-05-09 22:06:16 -0300

    Exercício 17. A terminar.

--- Entra21.ExerciciosWhile/Exercício17.cs
+++ Entra21.ExerciciosWhile/Exercício17.cs
@@ -11,16 +11,22 @@ namespace Entra21.ExerciciosWhile
         public void Executar()
         {
             Console.Write("Temporizador: ");
-            int tempoSegundos = 86399;
-            int indiceSegundo = 0;
-            while (indiceSegundo < 86399)
+            int segundos = 59, minutos = 59, horas = 23;
+            int indiceSegundos = 0, indiceMinutos = 0, indiceHoras = 0;
+            int indice = 0;
+            while (indice < 86399)
             {
-                //Horas
-                Console.WriteLine((tempoSegundos / 60) / 60);
-                //Minutos
-
-                tempoSegundos = tempoSegundos - 1;
-                indiceSegundo = indiceSegundo + 1;
+                Console.Write(horas + ":" + minutos + ":" + segundos);
+                segundos = segundos - 1;
+                if ((segundos < 0))
+                {
+                    segundos = 59;
+                }
+                else
+                { }
+                indiceMinutos = indiceMinutos + 1;
+                if ((indiceMinutos%3600)  1 )
+                indice = indice + 1;
             }
         }
     }
