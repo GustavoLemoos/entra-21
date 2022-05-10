4a01eb191e45dc47dfbd6f1a15bc6acec59a3fff
GustavoLemoos <gustavo.aulas@outlook.com>
2022-05-10 16:27:11 -0300

    Exercício 12. Início da resolução do exercício 12.

--- Entra21.ExerciciosWhile/Program.cs
+++ Entra21.ExerciciosWhile/Program.cs
@@ -1,15 +1,16 @@
 ﻿using Entra21.ExerciciosWhile;
 
 Console.WriteLine(@"---------Menu--------------
-1 - Exercício 08
-2 - Exercício 09
-3 - Exercício 10
-4 - Exercício 13
-5 - Exercício 14
-6 - Exercício 15
-7 - Exercício 16
-8 - Exercício 17
-9 - Exercício 11");
+1  - Exercício 08
+2  - Exercício 09
+3  - Exercício 10
+4  - Exercício 13
+5  - Exercício 14
+6  - Exercício 15
+7  - Exercício 16
+8  - Exercício 17
+9  - Exercício 11
+10 - Exercício 12");
 
 Console.Write("Digite a opção desejada: ");
 int opcaoDesejada = Convert.ToInt32(Console.ReadLine());
@@ -59,4 +60,9 @@ else if (opcaoDesejada == 9)
 {
     Exercício11 exercicio11 = new Exercício11();
     exercicio11.Executar();
+}
+else if (opcaoDesejada == 10)
+{
+    Exercício12 exercicio12 = new Exercício12();
+    exercicio12.Executar();
 }
