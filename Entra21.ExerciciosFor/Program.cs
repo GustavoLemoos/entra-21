8a21cb691224514007be869033628c9caf936abe
Gustavo Lemos <gustavo.aulas@outlook.com>
2022-05-13 21:32:24 -0300

    Exercício 01. Resolução parcial do exercício 01.

---
+++ Entra21.ExerciciosFor/Program.cs
@@ -0,0 +1,15 @@
+﻿using Entra21.ExerciciosFor;
+
+var opcaoDesejada = 0;
+Console.WriteLine(@"---------Menu--------------
+1 - Exercício 01
+2 - Exercício 02");
+Console.WriteLine("Digite a opção desejada: ");
+opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+
+if (opcaoDesejada == 1)
+{
+    // MenuDaClasse nomeVariavel = new NomeDaClasse();
+    Exercicio01 exercicio01 = new Exercicio01();
+    exercicio01.Executar();
+}
