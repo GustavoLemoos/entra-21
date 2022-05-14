8ed37bb7d7bacac28094bba9e2cf4ecf461d945c
Gustavo Lemos <gustavo.aulas@outlook.com>
2022-05-13 21:52:21 -0300

    Exercício05. Exercício 05 iniciado.

---
+++ Entra21.ExerciciosFor/Exercicio02.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosFor
+{
+    internal class Exercicio02
+    {
+        public void Executar()
+        {
+            var quantidadeCarrosCadastrar = 0;
+            var anoCarro = 0;
+            var modelo = "";
+            var valorCarro = 0.00;
+            var nomeComecaG = 0;
+            var nomeComecaA = 0;
+            Console.Write("Digite a quantidade de carros à cadastrar: ");
+            quantidadeCarrosCadastrar = Convert.ToInt32(Console.ReadLine());
+            for (var i = 0; indice < quantidadeCarrosCadastrar; i++)
+            {
+                Console.WriteLine("Digite o modelo do carro: ");
+                modelo = Console.ReadLine();
+                Console.WriteLine("Digite o valor do carro: R$");
+                valorCarro = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Digite o ano do carro: ");
+                anoCarro = Convert.ToInt32(Console.ReadLine());
+                if (modelo.Trim().ToLower().StartsWith("g") == true)
+                {
+
+                }
+            }
+
+
+        }
+    }
+}
--- Entra21.ExerciciosFor/Program.cs
+++ Entra21.ExerciciosFor/Program.cs
@@ -3,7 +3,8 @@
 var opcaoDesejada = 0;
 Console.WriteLine(@"---------Menu--------------
 1 - Exercício 01
-2 - Exercício 02");
+2 - Exercício 02
+3 - Exercício 03");
 Console.WriteLine("Digite a opção desejada: ");
 opcaoDesejada = Convert.ToInt32(Console.ReadLine());
 
@@ -12,4 +13,10 @@ if (opcaoDesejada == 1)
     // MenuDaClasse nomeVariavel = new NomeDaClasse();
     Exercicio01 exercicio01 = new Exercicio01();
     exercicio01.Executar();
+}
+if (opcaoDesejada == 1)
+{
+    // MenuDaClasse nomeVariavel = new NomeDaClasse();
+    Exercicio02 exercicio02 = new Exercicio02();
+    exercicio02.Executar();
 }
