d4859c8c40becd0ffb92a0dbac897149295778c4
GustavoLemoos <gustavo.aulas@outlook.com>
2022-05-15 15:53:37 -0300

    Exercício02 for. Exercício desenvolvido.

---
+++ Entra21.ExerciciosFor2/Exercicio01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosFor2
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            var nomePeca = "";
+            var precoPeca = 0.00;
+            for (var i = 0; i < 13; i++)
+            {
+                Console.WriteLine("Digite o nome da peça: ");
+                nomePeca = Console.ReadLine();
+                Console.WriteLine("Digite o preço da peça: R$");
+                precoPeca = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+    }
+}
