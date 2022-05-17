2f1ebdebf35b5d7d33ccd4881049b0f0ae57a289
GustavoLemoos <gustavo.aulas@outlook.com>
2022-05-16 21:03:51 -0300

    Exercício 01. Desenvolvido.

--- Entra21.ExerciciosFor2/Exercicio01.cs
+++ Entra21.ExerciciosFor2/Exercicio01.cs
@@ -10,14 +10,38 @@ namespace Entra21.ExerciciosFor
     {
         public void Executar()
         {
-            var nomePeca = "";
-            var precoPeca = 0.00;
             for (var i = 0; i < 13; i++)
             {
+                var nomePeca = "";
+                var nomeValido = false;
+                var precoPeca = 0.00;
                 Console.WriteLine("Digite o nome da peça: ");
                 nomePeca = Console.ReadLine();
-                Console.WriteLine("Digite o preço da peça: R$");
-                precoPeca = Convert.ToDouble(Console.ReadLine());
+                while (nomePeca.Trim().Length == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Digite um nome válido para a peça: ");
+                    nomePeca = Console.ReadLine();
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                while ((precoPeca <= 0.00))
+                {
+                    try
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("Digite o preço da peça: R$");
+                        precoPeca = Convert.ToDouble(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Digite um número válido para o preço da peça.");
+                    }
+                }
             }
         }
     }
