843375c2a8f1fdbfff9f92db84882eccb535a304
GustavoLemoos <gustavo.aulas@outlook.com>
2022-05-20 17:06:33 -0300

    Estrutura básica exercicios vetor.

--- Entra21.Vetor/Program.cs
+++ Entra21.Vetor/Program.cs
@@ -2,18 +2,50 @@
 
 var opcaoDesejada = 0;
 Console.WriteLine(@"---------Menu--------------
-1 - Exercício 01
-2 - Exercício 02");
+1 - Exercício 01 Sala
+2 - Exercício 02 Sala
+3 - Exercicio 01
+4 - Exercicio 02
+5 - Exercicio 03
+6 - Exercicio 04
+7 - Exercicio 05
+8 - Exercicio 06
+9 - Exercicio 07");
 Console.Write("Digite a opção desejada: ");
 opcaoDesejada = Convert.ToInt32(Console.ReadLine());
 
 if (opcaoDesejada == 1)
 {
-    Exercicio01Sala exercicio01 = new Exercicio01Sala();
-    exercicio01.Executar();
+    Exercicio01Sala exercicio01Sala = new Exercicio01Sala();
+    exercicio01Sala.Executar();
 }
 if (opcaoDesejada == 2)
 {
-    Exercicio02Sala exercicio02 = new Exercicio02Sala();
-    exercicio02.Executar();
+    Exercicio02Sala exercicio02Sala = new Exercicio02Sala();
+    exercicio02Sala.Executar();
+}
+if (opcaoDesejada == 3)
+{
+    Exercicio01 exercicio01 = new Exercicio01();
+    exercicio01.Executar();
+}
+if (opcaoDesejada == 4)
+{
+    Exercicio04 exercicio04 = new Exercicio04();
+    exercicio04.Executar();
+}
+if (opcaoDesejada == 5)
+{
+    Exercicio05 exercicio05 = new Exercicio05();
+    exercicio05.Executar();
+}
+if (opcaoDesejada == 6)
+{
+    Exercicio06 exercicio06 = new Exercicio06();
+    exercicio06.Executar();
+}
+if (opcaoDesejada == 7)
+{
+    Exercicio07 exercicio07 = new Exercicio07();
+    exercicio07.Executar();
 }
