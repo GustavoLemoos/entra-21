df08e468e3deba2e4c9da55aa36109c1364ad3b0
joaomarti755 <joaomarti755@gmail.com>
2022-05-20 18:21:14 -0300

    Exercicio01 vetor. Desenvolvido.

--- Entra21.Vetor/Exercicio01.cs
+++ Entra21.Vetor/Exercicio01.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Crie um vetor para guardar 16 números inteiros.
+//Preencha todas as posições com números e apresente a soma destes números.
+
+
 namespace Entra21.Vetor
 {
     internal class Exercicio01
     {
         public void Executar()
         {
-
+            int[] numeros = new int[16];
+            int soma = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                Console.Write("Digite um número: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                numeros[i] = numero;
+                soma = soma + numeros[i];
+            }
+            Console.Write($"A soma dos números é {soma}");
         }
     }
 }
