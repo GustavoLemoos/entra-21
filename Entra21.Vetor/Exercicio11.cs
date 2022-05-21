96bfacd2809e5c1b707c8fca7e06f6c6dbce783a
GustavoLemoos <gustavo.aulas@outlook.com>
2022-05-21 15:23:58 -0300

    Exercicio04 vetor complementar. Desenvolvido.

--- Entra21.Vetor/Exercicio11.cs
+++ Entra21.Vetor/Exercicio11.cs
@@ -15,7 +15,58 @@ namespace Entra21.Vetor
     {
         public void Executar()
         {
+            int quantidade = 10, quantidadePares = 0, quantidadeImpares = 0;
+            int[] numeros = new int[quantidade];
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"Digite um número inteiro: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                numeros[i] = numero;
+                if ((numero % 2) == 0)
+                {
+                    quantidadePares++;
+                }
+                else
+                {
+                    quantidadeImpares++;
+                }
+            }
 
+            int[] numerosPares = new int[quantidadePares];
+            int posicaoPar = 0;
+            int[] numerosImpares = new int[quantidadeImpares];
+            int posicaoImpar = 0;
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                if ((numeros[i] % 2) == 0)
+                {
+                    numerosPares[posicaoPar] = numeros[i];
+                    posicaoPar++;
+                }
+                else
+                {
+                    numerosImpares[posicaoImpar] = numeros[i];
+                    posicaoImpar++;
+                }
+            }
+
+            Console.Write("O vetor numeros armazena: ");
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"{numeros[i]}, ");
+            }
+
+            Console.Write("\nO vetor numerosPares armazena: ");
+            for (var i = 0; i < numerosPares.Length; i++)
+            {
+                Console.Write($"{numerosPares[i]}, ");
+            }
+
+            Console.Write("\nO vetor numerosImpares armazena: ");
+            for (var i = 0; i < numerosImpares.Length; i++)
+            {
+                Console.Write($"{numerosImpares[i]}, ");
+            }
         }
     }
 }
