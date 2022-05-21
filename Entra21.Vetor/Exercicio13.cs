e527e6c06f12198f40bef2572885ca5444d1f3ad
GustavoLemoos <gustavo.aulas@outlook.com>
2022-05-21 16:20:57 -0300

    Exercicios 12 e 13 vetores complementar. Desenvolvidos.

--- Entra21.Vetor/Exercicio13.cs
+++ Entra21.Vetor/Exercicio13.cs
@@ -14,7 +14,37 @@ namespace Entra21.Vetor
     {
         public void Executar()
         {
+            int quantidade = 10;
+            double[] numeros = new double[quantidade];
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                Console.Write("Digite um número: ");
+                double numero = Convert.ToDouble(Console.ReadLine());
+                numeros[i] = numero;
+            }
 
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                for (var j = 0; j < numeros.Length; j++)
+                {
+                    if (numeros[i] >= numeros[j])
+                    {
+                        double numeroAux1 = numeros[j];
+                        double numeroAux2 = numeros[i];
+                        numeros[i] = numeroAux1;
+                        numeros[j] = numeroAux2;
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+
+            Console.Write("Os números em ordem decrescente são: ");
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"{numeros[i]}, ");
+            }
         }
     }
 }
