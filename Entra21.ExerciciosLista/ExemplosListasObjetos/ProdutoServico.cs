8d03ad1a3363607dc0e756ec04e2b896867a0e96
Gustavo Lemos <gustavo.aulas@outlook.com>
2022-06-09 19:20:05 -0300

    Aula.

--- Entra21.ExerciciosLista/ExemplosListasObjetos/ProdutoServico.cs
+++ Entra21.ExerciciosLista/ExemplosListasObjetos/ProdutoServico.cs
@@ -31,9 +31,24 @@ namespace Entra21.ExerciciosLista.ExemplosListasObjetos
 
         }
 
-        public void Apagar()
+        public void Apagar(int codigo)
         {
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                // Resgatando de uma lista de tipos primitivos
+                // int numero = numeros[i];
 
+                // Resgatando de uma lista de objetos
+                Produto produto = produtos[i];
+                if (produto.Codigo == codigo)
+                {
+                    // Posso remover com o indice do objeto desejado
+                    //produtos.RemoveAt(i);
+
+                    // Posso remover com o objeto desejado
+                    produtos.Remove(produto);
+                }
+            }
         }
 
         public List<Produto> ObterTodos()
@@ -41,8 +56,23 @@ namespace Entra21.ExerciciosLista.ExemplosListasObjetos
             return produtos;
         }
 
-        public Produto ObterCodigo()
+        public Produto ObterPorCodigo(int codigo)
         {
+            // Percorrer todos os elementos para encontrar o produto filtrando por código
+            for (int i = 0; i < produtos.Count; i++)
+            {
+                // Obter o produto da lista de produtos em determinado indice
+                var produtoAtual = produtos[i];
+
+                // Verifica se o produto atual da lista percorrida é o desejado
+                if (produtoAtual.Codigo == codigo)
+                {
+                    // Retorna o produto encontrado
+                    return produtoAtual;
+                }
+            }
+
+            // Retorna null, caso não encontre um produto com o código passado como parâmetro
             return null;
         }
     }
